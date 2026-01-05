4756a7197d49ed97a382e6cf84cd2b3b34e6031e
Akhmadjon007 <ahmadovahmadjonqsh125@gmail.com>
2026-01-05 13:05:31 +0500

    smth added to index.js

--- 7-dars/index.js
+++ 7-dars/index.js
@@ -63,39 +63,192 @@
 //  console.log(Product.byPrice())
 //  console.log(Product.byWeight())
 
-// 3-masala ////////////////////////////////////////////////////////////////////////////////
+// // 3-masala ////////////////////////////////////////////////////////////////////////////////
 
 // class User {
 //     static users=[]
 
 //     constructor(username, password){
-//         if(!users.some(v=>v===username)){
+//         if(!User.users.some(v=>v.username===username)){
 //             this.username=username
 //             this.password=password
 
 //             User.users.push(this)
 //         }else{
-//             console.log("Bu user band. bohsqasini kiriting");
+//             console.log(`${username} useri band. bohsqasini kiriting`);
 //         }
 //     }
 
 
 //     static login(username, password){
-//         if(users.some(v => {
-//             v.username=username
-//             v.password=password
-//         })){
-//             return 'xush kelibsiz'
+//         if(User.users.some(v => 
+//             v.username==username &&
+//             v.password==password
+//         )){
+//             console.log('xush kelibsiz')
 //         }else{
-//             "Login yoki parol xato"
+//             console.log("Login yoki parol xato")
 //         }
+//     }
+
+//     static getUsers(){
+//         return this.users
+//     }
+    
+// }
+
+// let user1=new User('ahmadjon', 12345678)
+// let user2=new User('ali', 11111111)
+// let user3=new User('ahmadjon', 20010707) // user band deb chiqadi
+
+
+// console.log(User.getUsers());
+
+
+// User.login('ahmadjon', 12345678)
+// User.login('ali', 11111111)
+// User.login('ahmadjon', 20010707)
+
+// // 4-masala ////////////////////////////////////////////////////////////////////////////////
+
+// class Flight {
+//     static seatsTemple() {
+//         let seatsTemple={}
+//         for (let i = 1; i <= 25; i++) {
+//             seatsTemple[i]=null        
+//         }
+//         return seatsTemple
+//     }
+
+//     constructor(flightNumber, departureTime, destination){
+//         this.flightNumber=flightNumber
+//         this.departureTime=departureTime
+//         this.destination=destination
+//         this.seats={...Flight.seatsTemple()}
+//     }
+
+//     bookSeat(seatNum, name){
+//         if(!this["seats"][seatNum]){
+//             this["seats"][seatNum]=name
+//             console.log(`${seatNum}-o'rindiq ${name} nomiga rasmiylashtirildi`)
+//         }else{
+//             console.log(`${seatNum}-o'rindiq band`)
+//         }
+//     }
+
+//     cancelSeat(seatNum){
+//         if(this["seats"][seatNum]){
+//             this["seats"][seatNum]=null
+//             console.log(`${seatNum}-o'rindiq uchun buyurtma bekor qilindi`)
+//         }else{
+//             console.log(`${seatNum}-o'rindiq allaqachon bo'sh`)
+//         }
+//     }
+
+//     getAvailableSeats(){
+//         let available=[]
+//         for(const k in this.seats){
+//             if(!this['seats'][k]) available.push(k)
+//         }
+//     console.log(`bo'sh o'rindiqlar: ${available.join(', ')}`)
 //     }
 // }
 
+// let fligt1=new Flight(1, 1200, 'BAA')
+// fligt1.bookSeat(1, "Ahmadov Ahmadjon")
+// fligt1.bookSeat(3, "Ali Valiyev")
+// fligt1.bookSeat(1, "Alisher Navoiy")
+// fligt1.cancelSeat(3)
+// fligt1.cancelSeat(3)
+// fligt1.cancelSeat(15)
+// fligt1.getAvailableSeats()
 
 
+// // 5-masala ////////////////////////////////////////////////////////////////////////////////
 
-// 4-masala ////////////////////////////////////////////////////////////////////////////////
-// 5-masala ////////////////////////////////////////////////////////////////////////////////
-// 6-masala ////////////////////////////////////////////////////////////////////////////////
-// 7-masala ////////////////////////////////////////////////////////////////////////////////
+// class Course{
+//     constructor(name, code){
+//         this.courseName=name
+//         this.courseCode=code
+//         this.students={}
+//     }
+
+//     addStudent(id, name){
+//         this['students'][id]=name
+//     }
+
+//     removeStudent(id){
+//         if(this['students'][id]) {
+//             delete this['students'][id]
+//         }
+//     }
+
+//     getStudents(){
+//         return this.students
+//     }
+// }
+
+// let matematika=new Course('Beginner', 11030)
+// matematika.addStudent(254, 'Ali')
+// matematika.addStudent(126, 'Vali')
+// console.log(matematika.getStudents())
+// matematika.removeStudent(254)
+// console.log(matematika.getStudents())
+
+
+// // 6-masala ////////////////////////////////////////////////////////////////////////////////
+
+// class Wallet{
+//     constructor(money=0){
+//         this.balance=Number(money)
+//     }
+
+//     spendMoney(money){
+//         if(money<=this.balance){
+//             this.balance-=money
+//         }else{
+//             console.log("Xisobingizda yetarli mablag' mavjud emas");
+//         }
+//     }
+
+//     addMoney(money){
+//         this.balance+=Number(money)
+//     }
+
+//     getBalance(){
+//         return this.balance
+//     }
+// }
+
+// let kiwi = new Wallet('900')
+// console.log(kiwi.getBalance());
+// kiwi.addMoney(400)
+// kiwi.spendMoney(800)
+// console.log(kiwi.getBalance());
+// kiwi.addMoney(50)
+// kiwi.spendMoney(600)
+// console.log(kiwi.getBalance());
+
+// // 7-masala ////////////////////////////////////////////////////////////////////////////////
+
+
+// class Notification{
+//     constructor(message, date){
+//         this.message=message
+//         this.date=date
+//     }
+
+//     get sendNotification(){
+//         console.log(this.message);
+//     }
+
+//     clearNotification(){
+//         this.message=''
+//     }
+// }
+
+// let txt=new Notification('Uyga vazifa berildi', Date.now()+24*60*60*1000)
+
+// txt.sendNotification
+// txt.clearNotification()
+// console.log(txt)
